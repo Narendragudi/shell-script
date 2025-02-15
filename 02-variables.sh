87b840c444433a3ec06b77338f7a45c6e0f0eb1d
Narendra Gudi <narendragudi7799@gmail.com>
2025-02-15 10:20:53 +0530

    shell scripting

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh::good morning suresh, how are you"
+echo "Suresh::good morning ramesh, i am good, what about you"
+echo "Ramesh::i am fine suresh, what's next"
+echo "Suresh::nothing ramesh"
