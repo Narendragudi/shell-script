4a8db58d9116bfb11168f2dfe75fbe4a027c55ac
Narendragudi <narendragudi7799@gmail.com>
2025-02-18 22:49:18 +0530

    added shell scripting

--- 09-install-packages.sh
+++ 09-install-packages.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 ID=$(id -u)
+R="/e[31m"
+G="/e[32m"
+N="/e[0m"
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR:: please run this script with root access"
+    echo -e "$R ERROR:: please run this script with root access $N"
     exit 1
 else
     echo "you are root user"
