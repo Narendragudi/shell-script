ffecc6efbf7f173dd76cb1bcf0da8f14bd5b0f67
Narendra Gudi <narendragudi7799@gmail.com>
2025-02-18 19:53:05 +0530

    added shell script

---
+++ 07-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "script name: $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ] 
+    then
+        echo "ERROR:: $2 .... failed"
+        exit 1
+    else
+        echo "$2 .... SUCCESS"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: please run this script with root access"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+yum install mysql -y
+
+VALIDATE $? "Installing MYSQL"
+
+yum install git -y
+
+VALIDATE $? "Installing GIT"
