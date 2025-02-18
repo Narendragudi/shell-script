4a3e8e1b3aab11cadaa47a2c21eb7ccc6b15afc1
Narendragudi <narendragudi7799@gmail.com>
2025-02-18 23:26:57 +0530

    added shell-script

--- 09-install-packages.sh
+++ 09-install-packages.sh
@@ -3,7 +3,21 @@
 ID=$(id -u)
 R="/e[31m"
 G="/e[32m"
+Y="/e[33m"
 N="/e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing ar $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE (){
+    [$1 -ne 0 ]
+    then 
+    echo -e "$2 ...$R FAILED $N"
+    else
+    echo -e "$2 ...$G SUCCESS $N"
+}
 
 if [ $ID -ne 0 ]
 then
@@ -13,4 +27,15 @@ else
     echo "you are root user"
 fi
 
-echo "All arguments passed: $@"
+#echo "All arguments passed: $@"
+for package in $@
+do
+    yum list installed $package &>> $LOGFILE
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y &>> $LOGFILE
+        VALIDATE $? "installition of $package"
+    else
+        echo -e "package is already installed ... $Y SKIPPING $N"
+    fi
+done
